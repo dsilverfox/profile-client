6497c3f82430ddcbe1db991650bbca0d6c0039ab
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-03-01 15:35:43 -0500

    Started coding for contact page, created build in progress page.

---
+++ src/components/InProgress.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InProgress = () => {
+
+    return(
+        <div>
+            This build is currently in progress.
+        </div>
+    )
+}
+
+export default InProgress
---
+++ src/components/contacts.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+
+const ContactForm = () => {
+    const [firstname, setFirstName] = useState('')
+    const [lastname, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [company, setCompany] = useState('')
+    const [message, setMessage] = useState('')
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
