d3f563f72c105d1361873ca61b96966c52be9216
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-03-01 17:56:08 -0500

    Form submission sends email and adds to database, will handle route out later.

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 const ContactForm = () => {
     const [firstname, setFirstName] = useState('')
@@ -8,9 +8,15 @@ const ContactForm = () => {
     const [phone, setPhone] = useState('')
     const [company, setCompany] = useState('')
     const [message, setMessage] = useState('')
+    const [state, handleSubmit] = useForm('mbjwyojo');
 
-    const formSubmit = (e) => {
-        e.preventDefault();
+ function ContactForm () {
+        if (state.succeeded) {
+            return <div>Thank you for signing up!</div>;
+        }
+    }
+
+    const formSubmit = () => {
         fetch("http://localhost:3000/contact/contact", {
             method: "POST",
             body: JSON.stringify({
@@ -32,41 +38,90 @@ const ContactForm = () => {
 
     return (
         <div>
-            <section className="contact">
-                    <h1>Contact Me</h1>
-                    <center><p>Interested in reaching me? I can be reached using the links in the footer on specific social media or you may email me using this contact form. Please include how you wish to be contacted because, unless I know you are calling I will not answer.</p> </center>
-                <form action="https://formspree.io/f/mbjwyojo" method="POST">
-                        <label>First Name: <input type="text" 
-                        value={firstname}onChange={(e) => setFirstName(e.target.value)}/> </label>
-                    <label>Last Name: <input type="text"
-                    value={lastname} 
-                    onChange={(e)=>setLastName(e.target.value)}/></label>
-                        <label>Company: <input type="text
-                        "value={company} 
-                        onChange={(e)=>setCompany(e.target.value)}/></label>
-                        <label>
-                            Your email:
-                            <input type="email" name="_replyto"
-                            value={email} 
-                            onChange={(e) => setEmail(e.target.value)}/>
-                        </label>
-                        <label>Your Phone: <input type="tel"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}/>
-                        </label>
-                            <label>Call? <input type="checkbox"/></label>
-                            <label>Text? <input type="checkbox"/></label>
-                            <label>Email? <input type="checkbox"/></label>
-                            <label>
-                                Your message:
-                                <textarea name="message"
-                                type="textarea"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}></textarea>
-                            </label>
-                            <button type="submit" onClick={formSubmit}>Send</button>
-                    </form>
-                    </section>
+                <form onSubmit={handleSubmit}>
+                    
+                    <label htmlFor='firstname'>First Name:</label>
+                    <input
+                        id="firstname"
+                        name="firstname"
+                        type="text"
+                        value={firstname}
+                        onChange={(e) => setFirstName(e.target.value)} />
+                    <ValidationError
+                        prefix="firstname"
+                        field="firstname"
+                        errors={state.errors}
+                    />
+
+                    <label htmlFor='lastname'>Last Name:</label>
+                    <input
+                        id="lastname"
+                        name="lastname"
+                        type="text"
+                        value={lastname}
+                        onChange={(e) => setLastName(e.target.value)} />
+                    <ValidationError
+                        prefix="lastname"
+                        field="lastname"
+                        errors={state.errors}
+                    />
+
+
+                    <label htmlFor='company'>Company:</label> 
+                    <input 
+                    id="company"
+                    name="company"
+                    type="text"
+                    value={company}
+                    onChange={(e) => setCompany(e.target.value)} />
+                    <ValidationError
+                    prefix="company"
+                    field="company"
+                    errors={state.errors}
+                    />
+
+                    <label htmlFor='email'>Your email:</label>
+                    <input 
+                    id="email"
+                    type="email" 
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                    <ValidationError
+                    prefix="email"
+                    field="email"
+                    errors={state.errors}
+                    />
+                    
+                    <label htmlFor='phone'>Your Phone:</label>
+                    <input 
+                    id="phone"
+                    name="phone"
+                    type="tel"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)} />
+                    <ValidationError
+                    prefix="phone"
+                    field="phone"
+                    errors={state.errors}
+                    />
+
+
+                    <label htmlFor='message'> Your message:</label>
+                    <textarea
+                    id="message" 
+                    name="message"
+                    type="textarea"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}/>
+                    <ValidationError
+                    prefix="message"
+                    field="message"
+                    errors={state.errors}
+                    />
+                    
+                    <button type="submit" onClick={formSubmit} disabled={state.submitting}>Send</button>
+                </form>
         </div>
     )
 }
